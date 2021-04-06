1811370f271b340cac66fef500c7051ce36209ff
Justin Hibbits <chmeeedalf@gmail.com>
2021-04-06 15:51:48 -0500

    Allow customizing linenoise buffer size

--- 3rdparty/linenoise/linenoise.c
+++ 3rdparty/linenoise/linenoise.c
@@ -118,7 +118,9 @@
 #include "linenoise.h"
 
 #define LINENOISE_DEFAULT_HISTORY_MAX_LEN 100
+#ifndef LINENOISE_MAX_LINE
 #define LINENOISE_MAX_LINE 4096
+#endif
 static char *unsupported_term[] = {"dumb","cons25","emacs",NULL};
 static linenoiseCompletionCallback *completionCallback = NULL;
 static linenoiseHintsCallback *hintsCallback = NULL;
