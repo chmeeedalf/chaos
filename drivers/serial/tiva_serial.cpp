0864552f2394d82b4a5198073b236203ba42d30f
Justin Hibbits <chmeeedalf@gmail.com>
2021-09-07 15:17:36 -0500

    Remove array designators from Tiva serial driver
    These are a C99-ism, and not really supported in C++.

--- drivers/serial/tiva_serial.cpp
+++ drivers/serial/tiva_serial.cpp
@@ -27,7 +27,7 @@ struct tiva_uart_gpio_config {
 };
 
 static struct tiva_uart_gpio_config valid_configs[] = {
-	[0] = { .tx = {GPIO_PA1},
+	{ .tx = {GPIO_PA1},
 		.rx = {GPIO_PA0},
 		.cts = {GPIO_PH1, GPIO_PM4, GPIO_PB4},
 		.rts = {GPIO_PH0, GPIO_PB5},
@@ -35,7 +35,7 @@ static struct tiva_uart_gpio_config valid_configs[] = {
 		.dsr = {GPIO_PH3, GPIO_PM6, GPIO_PP4},
 		.dtr = {GPIO_PP2},
 		.ri = {GPIO_PK7, GPIO_PM7}},
-	[1] = { .tx = {GPIO_PB1},
+	{ .tx = {GPIO_PB1},
 		.rx = {GPIO_PB0, GPIO_PQ4},
 		.cts = {GPIO_PP3, GPIO_PN1},
 		.rts = {GPIO_PE0, GPIO_PN0},
@@ -43,23 +43,23 @@ static struct tiva_uart_gpio_config valid_configs[] = {
 		.dsr = {GPIO_PE1, GPIO_PN3},
 		.dtr = {GPIO_PE3, GPIO_PN4},
 		.ri = {GPIO_PN5, GPIO_PE4}},
-	[2] = { .tx = {GPIO_PA7, GPIO_PD5},
+	{ .tx = {GPIO_PA7, GPIO_PD5},
 		.rx = {GPIO_PA6, GPIO_PD4},
 		.cts = {GPIO_PN3, GPIO_PD7},
 		.rts = {GPIO_PN2, GPIO_PD6}},
-	[3] = { .tx = {GPIO_PA5, GPIO_PJ1},
+	{ .tx = {GPIO_PA5, GPIO_PJ1},
 		.rx = {GPIO_PA4, GPIO_PJ0},
 		.cts = {GPIO_PP3, GPIO_PN5},
 		.rts = {GPIO_PP4, GPIO_PN4}},
-	[4] = { .tx = {GPIO_PK1, GPIO_PA3},
+	{ .tx = {GPIO_PK1, GPIO_PA3},
 		.rx = {GPIO_PK0, GPIO_PA2},
 		.cts = {GPIO_PK3},
 		.rts = {GPIO_PK2}},
-	[5] = { .tx = {GPIO_PC7},
+	{ .tx = {GPIO_PC7},
 		.rx = {GPIO_PC6}},
-	[6] = { .tx = {GPIO_PP1},
+	{ .tx = {GPIO_PP1},
 		.rx = {GPIO_PP0}},
-	[7] = { .tx = {GPIO_PC5},
+	{ .tx = {GPIO_PC5},
 		.rx = {GPIO_PC4}}
 };
 
