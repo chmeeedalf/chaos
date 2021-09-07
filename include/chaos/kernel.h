0b4a37060f1cd9c386c87679538c940ac8a4b5fb
Justin Hibbits <chmeeedalf@gmail.com>
2021-09-07 15:21:08 -0500

    Add linker set macro, drop __section macro from kernel header
    Use cdefs.h from newlib for base macros, since it's now imported.  This
    gives us __section().
    
    Add linker_set macros, to make it easier to create linker sets.

--- include/chaos/kernel.h
+++ include/chaos/kernel.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_SYS_KERNEL_H
 #define INCLUDE_SYS_KERNEL_H
 
+#include <sys/cdefs.h>
 #include <stdint.h>
 
 #ifndef MIN
@@ -20,8 +21,14 @@ uint32_t get_freq(void);
 }
 #endif
 
-#ifndef __section
-#define __section(x)	__attribute__((section(x)))
+#ifndef linker_set
+#define	linker_set(name, type)	\
+	type __set_##name##_start __section("." name ".A");	\
+	type __set_##name##_end __section("." name ".z");		\
+	struct hack
+
+#define	linker_set_elm(lname, type, name) \
+	type name __section("." lname "._." #name)
 #endif
 
 #endif
